95162e65e5d08d642944e7782affe1c2fc3c6108
thomas <thomas@ThomasPC.local>
2025-07-05 16:16:38 -0500

    basically did nothing

--- first_stack/src/App.jsx
+++ first_stack/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <div className='login-container'>
             <div className='login-wrapper'>
               <div className='prompt'>
-                Enter Username: <input></input>
+                <h5 className='input-prompt'>Enter Username: </h5><input className='name-input'></input>
               </div>
             </div>
           </div>
